f98d7583807368cf148480f243c90929193ae207
ferNandoedo21 <fernandondiego17@gmail.com>
2024-03-22 23:08:26 -0600

    prueba

---
+++ include/Abono.h
@@ -0,0 +1,21 @@
+#ifndef ABONO_H
+#define ABONO_H
+#include "Fecha.h"
+#include <iostream>
+using namespace std;
+
+class Abono
+{
+    public:
+        Abono(Fecha*,float);
+        virtual ~Abono();
+        Fecha getFechaAbono();
+        float getMontoAbono();
+    protected:
+
+    private:
+        Fecha *fechaAbono;
+        float montoAbono;
+};
+
+#endif // ABONO_H
---
+++ src/Cliente.cpp
@@ -0,0 +1,22 @@
+#include "Cliente.h"
+
+Cliente::Cliente(int id,string n,string a)
+{
+    IdCliente=id;
+    nombre=n;
+    apellido=a;
+}
+
+Cliente::~Cliente()
+{
+    //dtor
+}
+int Cliente::getIdCliente(){
+    return IdCliente;
+}
+string Cliente::getNombre(){
+    return nombre;
+}
+string Cliente::getApellido(){
+    return apellido;
+}
